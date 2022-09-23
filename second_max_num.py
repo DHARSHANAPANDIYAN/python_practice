1ed03da12a67283bef3f6170ce5080d1f0aeb40f
DHARSHANAPANDIYAN <84447901+DHARSHANAPANDIYAN@users.noreply.github.com>
2022-09-23 11:27:33 +0530

    Update second_max_num.py

--- second_max_num.py
+++ second_max_num.py
@@ -1,16 +1,17 @@
 
+
 def find_runner_up(n,arr):
-    arr_new=list(set(list(arr)))
-    arr_new.sort()
-    first_place=max(arr_new)
-    arr_new.remove(first_place)
-    second_place=max(arr_new)
-    return second_place     
+    arr = list(set(arr))
+    arr.sort()
+    return arr[len(arr)-2]
+   
+     
     
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     res=find_runner_up(n,arr)
-    print(res)       
+    print(res)        
     # print(arr[arr.index(max(arr))]-1)
     
+
